dea44093b7ff19164ba9faff1e3229694552e1f0
Nishant Gerald <nishant.gerald@gmail.com>
2024-12-31 11:13:26 -0600

    setting dynamic PORT deployment

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, render_template, jsonify, request
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -134,4 +135,5 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if $PORT is not set
+    app.run(host="0.0.0.0", port=port)
